1ea86961eb335d152a5b7e0856789a0b312cd0c7
mazhars <mazhars@mitrlearning.com>
2018-03-19 20:12:12 +0530

    Structure and code rework

--- finder.js
+++ finder.js
@@ -1,64 +1,63 @@
 const $ = require('jquery');
 const find = require('find');
 const { dialog } = require('electron').remote;
-const error = require('./error');
-const findInFiles = require('./findInFiles');
+const findInFiles = require('find-in-files');
+const path = require('path');
+const search = require('recursive-search');
 
 const finder = {
   dir: '',
-  results: '',
-  elem: {
-    loader: $('.loader')[0],
-    results: $('.results')[0],
-    dir_button: $('.directory .button')[0],
-    dir_input: $('.directory .input')[0],
-    search_button: $('.search .button')[0],
-    search_input: $('.search .input')[0]
-  },
+  is_searching: false,
+  filter_ext: [],
+  filter_files: [],
 
   init() {
-    ['selectDirectory', 'directorySelected', 'search', 'filesFound', 'matchFound', 'searchComplete'].forEach((fn) => {
-      this[fn] = this[fn].bind(this);
-    });
-
-    $(this.elem.dir_button).on('click', this.selectDirectory);
-    $(this.elem.search_button).on('click', this.search);
+    $("#directory").on("click", finder.selectDirectory);
+    $("#search").on("click", finder.search);
   },
 
   selectDirectory() {
-    dialog.showOpenDialog({ properties: ['openDirectory'] }, this.directorySelected);
+    dialog.showOpenDialog({ properties: ['openDirectory'] }, finder.directorySelected);
   },
 
-  directorySelected(files = []) {
-    this.dir = files[0] || '';
-    this.elem.dir_input.value = this.dir;
+  directorySelected(folders = []) {
+    finder.dir = folders[0] || finder.dir;
+    $("#directory > input").val(finder.dir);
   },
 
   search() {
-    if (this.elem.dir_input.value === '') {
-      error.show('Please select a directory');
+    if ($("#directory > input").val() === '') {
       return false;
     }
 
-    if (this.elem.search_input.value === '') {
-      error.show('Please enter the text to search');
+    if ($("#search_text").val() === '') {
       return false;
     }
 
-    $(this.elem.loader).show();
-    this.results = '';
-    this.addResult();
+    finder.is_searching = true;
+    finder.search_filter = $("#search_filter").val().trim();
 
-    setTimeout(() => {
-      find.file(this.elem.dir_input.value, (files) => {
-        this.filesFound(files, this.elem.search_input.value);
-      });
-    }, 1000);
+    search.recursiveSearch('*', finder.dir, finder.onFileFound, finder.onSearchComplete);
+  },
+
+  onFileFound(err, file_path) {
+    const parsed_path = path.parse(file_path);
+    const filter = finder.search_filter;
+
+    if (filter && !(parsed_path.base.indexOf(filter) !== -1 || parsed_path.ext.indexOf(filter) !== -1)) {
+      return false;
+    }
+
+    console.log(file_path);
+  },
+
+  onSearchComplete(results) {
+    console.log('results');
   },
 
   filesFound(files = [], search_text) {
     $(this.elem.loader).hide();
-    findInFiles(files, search_text, this.matchFound, this.searchComplete);
+    // findInFiles(files, search_text, this.matchFound, this.searchComplete);
   },
 
   matchFound(file, occurences) {
--- main.js
+++ main.js
@@ -5,14 +5,15 @@ const path = require('path');
 let main_window = null;
 
 function createWindow() {
-  main_window = new BrowserWindow({ width: 800, height: 600, resizable: false, maximizable: false });
+  // main_window = new BrowserWindow({ height: 185, minHeight: 185, maxHeight: 185, minWidth: 800, maximizable: false });
+  main_window = new BrowserWindow({ height: 185, minHeight: 185, width: 800, minWidth: 800 });
 
   main_window.on('closed', function () {
     main_window = null;
   });
 
   main_window.loadURL(url.format({
-    pathname: path.resolve(__dirname, 'app.html'),
+    pathname: path.resolve(__dirname, 'finder.html'),
     protocol: 'file:',
     slashes: true
   }));
@@ -31,6 +32,6 @@ function createMenu() {
 }
 
 app.on('ready', function () {
-  createMenu();
+  // createMenu();
   createWindow();
 });
